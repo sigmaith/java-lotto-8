227a19978c67d28b5ddcd0fbb03fa3e632403073
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2025-11-03 13:31:13 +0900

    feat(input): 당첨번호, 보너스번호 입력

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
@@ -14,6 +15,21 @@ public class InputView {
     }
 
     public static List<Integer> getWinningNums() {
-        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String[] splits = Console.readLine().split(",");
+        try {
+            return Arrays.stream(splits).map(i -> Integer.parseInt(i.strip())).toList();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자 형식이어야 합니다.");
+        }
+    }
+
+    public static int getBonusNum() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        try {
+            return Integer.parseInt(Console.readLine().strip());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자 형식이어야 합니다.");
+        }
     }
 }
